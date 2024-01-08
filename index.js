e22ff2a766418c647324525a2f96d69ba095934e
Diallo Amadou Oury <dialloamadououry@MacBook-Pro-de-Diallo.local>
2024-01-08 16:16:46 +0100

    i change my css

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
-const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "world",
-    password: "Wally90",
-    port: 5432,
-});
+// const db = new pg.Client({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "world",
+//     password: "Wally90",
+//     port: 5432,
+// });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
